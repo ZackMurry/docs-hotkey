ff0009b54a9373c498529f99397cd49b7e8f052c
Zack Murry <zackk.murryy@gmail.com>
2022-12-18 16:29:02 -0600

    Update pages

--- web/frontend/node_modules/framer-motion/dist/es/easing/back.mjs
+++
@@ -1,9 +0,0 @@
-import { mirrorEasing } from './modifiers/mirror.mjs';
-import { reverseEasing } from './modifiers/reverse.mjs';
-
-const createBackIn = (power = 1.525) => (p) => p * p * ((power + 1) * p - power);
-const backIn = createBackIn();
-const backOut = reverseEasing(backIn);
-const backInOut = mirrorEasing(backIn);
-
-export { backIn, backInOut, backOut, createBackIn };
