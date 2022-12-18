23f980fa54fd5f92e1d66db79092773e1d22b160
Zack Murry <zackk.murryy@gmail.com>
2022-12-18 17:29:51 -0600

    Add TinyAnalytics

--- web/frontend/pages/_document.tsx
+++ web/frontend/pages/_document.tsx
@@ -12,6 +12,7 @@ export default class MyDocument extends Document {
           <meta name='theme-color' content={theme.colors.lightBlue} />
           <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap' />
           <link href='https://fonts.googleapis.com/css?family=DM Sans' rel='stylesheet' />
+          <script defer src="https://app.tinyanalytics.io/pixel/CTYlZeLC19CwBr7v"></script>
         </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
