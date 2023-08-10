1b6fca82f776862667a756f8025aaef53ac08e94
Zack Murry <zackk.murryy@gmail.com>
2023-08-10 15:45:24 -0500

    Document font weight

--- web/frontend/pages/font.tsx
+++ web/frontend/pages/font.tsx
@@ -21,6 +21,25 @@ const FontAction: FC = () => {
         list of recent fonts or in the longer list below it). The "More fonts" screen allows you to configure which fonts
         appear on the menu. Once your font is on the menu, simply type its name into the configuration box.
       </Text>
+      <Heading fontSize='24pt' color='headingText' mb='15px'>
+        Font Weight
+      </Heading>
+      <Text fontSize='13pt' color='bodyText' mb='15px'>
+        Additionally, some fonts have weights (e.g., "Thin", "Light", "Normal", "Medium", "Bold", and "Black") that specify
+        the thickness of the text beyond simply bold or unbold. The Font Weight action changes this property using the font
+        selection menu.
+      </Text>
+      <Img
+        src='/docs_font_weight_selection.png'
+        alt='Screenshot of the Google Docs font selection menu.'
+        width={{ base: '275px', md: '300px', lg: '325px' }}
+        height={{ base: '225px', md: '250px', lg: '275px' }}
+      />
+      <Text fontSize='13pt' color='bodyText' mb='15px' mt='15px'>
+        This action can set the font weight to any value using the configuration box. For example, using the "Thin"
+        configuration, the font weight would be set to thin. If either the font does not have the given weight or it does not
+        provide for different weights, this action cancels and does nothing.
+      </Text>
     </Box>
   )
 }
